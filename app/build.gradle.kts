4e18315676145a9197785ff4d856fcfa221abe07
mohamedali2285 <mohamedali2285@gmail.com>
2025-06-08 04:22:51 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.snakegameandroid"
-    compileSdk = 34
+    compileSdk = 34 // Explicitly specify compileSdkVersion
 
     defaultConfig {
         applicationId = "com.example.snakegameandroid"
