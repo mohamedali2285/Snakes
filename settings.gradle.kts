16c1e164fb7a45bab37126f222329270216f7a43
mohamedali2285 <mohamedali2285@gmail.com>
2025-06-08 03:53:07 +0200

    Initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.2.2" apply false
+        id("com.android.application") apply false
         id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     }
 }
